3bea1378c166ce73853035efb471f3402cd0e3e8
alvin <huang.alvin.01@gmail.com>
2021-04-07 13:04:08 -0700

    before merging main

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var router = express.Router();
 const { sessionCheck } = require("../auth");
-const { Bookshelf } = require("../db/models");
+const { Bookshelf, Book } = require("../db/models");
 
 /* GET home page. */
 router.get("/", sessionCheck, (req, res, next) => {
   const { id } = res.locals.user;
-  res.render("home(new)", { title: "a/A Express Skeleton Home" });
+  const latestBooks = Book.findAll({
+    limit: 9,
+    order: ["createdAt", "DESC"],
+  });
+  const bookshelf = Bookshelf.findOne({ where: { userId: id } });
+  const currentShelfCount = res.render("home(new)", {
+    title: "a/A Express Skeleton Home",
+  });
 });
 
 module.exports = router;
