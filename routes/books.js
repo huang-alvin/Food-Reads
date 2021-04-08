69ca463dba465fc3216b2ae2c8e35e69e5a1fbfe
alvin <huang.alvin.01@gmail.com>
2021-04-08 15:59:23 -0700

    WIP: conditionals for review and add to shelf

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { sessionCheck } = require("../auth");
 const { asyncHandler } = require("../utils");
-const { Book, Review } = require("../db/models");
+const { Book, Review, Bookshelf, Shelf } = require("../db/models");
+const { Op } = require("sequelize");
 
 // query the book object
 // display book image and its info & review.
@@ -14,25 +15,43 @@ const { Book, Review } = require("../db/models");
 // allow user to update/delete review
 // if user rating exist say update rating
 // else add a rating!
+// check if book is in bookshelf
+// else give a form w/ post route
 
 router.get(
   "/:id(\\d+)",
   sessionCheck,
   asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
     const { id } = req.params;
-    const books = await Book.findOne({ where: { id } });
+
+    const book = await Book.findOne({ where: { id } });
     const ratings = await Review.findAll({ where: { bookId: id } }).map(
       (rating) => rating.rating
     );
+
     let avgRating = 0;
+
     if (ratings.length) {
       avgRating =
         ratings.reduce((accum, currRating) => accum + currRating) /
         ratings.length;
     }
-
-    // res.json([books, ratings]);
-    res.render("book.pug", { books, ratings, avgRating });
+    const userRating = await Review.findOne({
+      where: { [Op.and]: [{ userId }, { bookId: id }] },
+    });
+    const userShelves = await Bookshelf.findAll({
+      where: {
+        userId,
+      },
+    });
+    // const hasBook = await Shelf.findOne({
+    //   where: {
+    //     [Op.and]: [{ bookId: id }, { [Op.or]: [{ bookshelfId: userShelves }] }],
+    //   },
+    // });
+    res.json([userRating, userShelves]);
+    // res.render("book.pug", { book, ratings, avgRating, userRating });
   })
 );
 
