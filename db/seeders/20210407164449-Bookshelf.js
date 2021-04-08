d649e0e28f7565f1a13a3e6cf319f6312d2cfbcd
Jorge Borges <jdborges187@gmail.com>
2021-04-08 11:04:32 -0400

    added bookshelves to first 9 users

--- db/seeders/20210407164449-Bookshelf.js
+++ db/seeders/20210407164449-Bookshelf.js
@@ -1,5 +1,15 @@
 'use strict';
 
+// const {User} = require('../models')
+
+// const helper = async () => {
+//   const users = await User.findAll();
+
+//   console.log(users);
+// }
+
+// helper();
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
@@ -15,10 +25,166 @@ module.exports = {
     return queryInterface.bulkInsert('Bookshelves', [
       {
       userId: 1,
-      status: 'Will-Read',
+      status: 'Want to Read',
       createdAt: new Date(),
       updatedAt: new Date()
-      }
+      },
+      {
+      userId: 1,
+      status: 'Currently Reading',
+      createdAt: new Date(),
+      updatedAt: new Date()
+      },
+      {
+      userId: 1,
+      status: 'Read',
+      createdAt: new Date(),
+      updatedAt: new Date()
+      },
+      {
+      userId: 2,
+      status: 'Want to Read',
+      createdAt: new Date(),
+      updatedAt: new Date()
+      },
+      {
+      userId: 2,
+      status: 'Currently Reading',
+      createdAt: new Date(),
+      updatedAt: new Date()
+      },
+      {
+      userId: 2,
+      status: 'Read',
+      createdAt: new Date(),
+      updatedAt: new Date()
+      },
+      {
+        userId: 3,
+        status: 'Want to Read',
+        createdAt: new Date(),
+        updatedAt: new Date()
+        },
+        {
+        userId: 3,
+        status: 'Currently Reading',
+        createdAt: new Date(),
+        updatedAt: new Date()
+        },
+        {
+        userId: 3,
+        status: 'Read',
+        createdAt: new Date(),
+        updatedAt: new Date()
+        },
+        {
+          userId: 4,
+          status: 'Want to Read',
+          createdAt: new Date(),
+          updatedAt: new Date()
+          },
+          {
+          userId: 4,
+          status: 'Currently Reading',
+          createdAt: new Date(),
+          updatedAt: new Date()
+          },
+          {
+          userId: 4,
+          status: 'Read',
+          createdAt: new Date(),
+          updatedAt: new Date()
+          },
+          {
+            userId: 5,
+            status: 'Want to Read',
+            createdAt: new Date(),
+            updatedAt: new Date()
+            },
+            {
+            userId: 5,
+            status: 'Currently Reading',
+            createdAt: new Date(),
+            updatedAt: new Date()
+            },
+            {
+            userId: 5,
+            status: 'Read',
+            createdAt: new Date(),
+            updatedAt: new Date()
+            },
+            {
+              userId: 6,
+              status: 'Want to Read',
+              createdAt: new Date(),
+              updatedAt: new Date()
+              },
+              {
+              userId: 6,
+              status: 'Currently Reading',
+              createdAt: new Date(),
+              updatedAt: new Date()
+              },
+              {
+              userId: 6,
+              status: 'Read',
+              createdAt: new Date(),
+              updatedAt: new Date()
+              },
+              {
+                userId: 7,
+                status: 'Want to Read',
+                createdAt: new Date(),
+                updatedAt: new Date()
+                },
+                {
+                userId: 7,
+                status: 'Currently Reading',
+                createdAt: new Date(),
+                updatedAt: new Date()
+                },
+                {
+                userId: 7,
+                status: 'Read',
+                createdAt: new Date(),
+                updatedAt: new Date()
+                },
+                {
+                  userId: 8,
+                  status: 'Want to Read',
+                  createdAt: new Date(),
+                  updatedAt: new Date()
+                  },
+                  {
+                  userId: 8,
+                  status: 'Currently Reading',
+                  createdAt: new Date(),
+                  updatedAt: new Date()
+                  },
+                  {
+                  userId: 8,
+                  status: 'Read',
+                  createdAt: new Date(),
+                  updatedAt: new Date()
+                  },
+                  {
+                    userId: 9,
+                    status: 'Want to Read',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                    },
+                    {
+                    userId: 9,
+                    status: 'Currently Reading',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                    },
+                    {
+                    userId: 9,
+                    status: 'Read',
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                    },
     ], {});
   },
 
